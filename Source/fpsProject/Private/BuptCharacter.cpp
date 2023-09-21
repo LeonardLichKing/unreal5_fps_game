264aa2953f1969c57b7edd278e217d150ed09b40
LeonardLichKing <LinChenQi@bupt.edu.cn>
2023-09-21 15:24:01 +0800

    Assert Useful

--- Source/fpsProject/Private/BuptCharacter.cpp
+++ Source/fpsProject/Private/BuptCharacter.cpp
@@ -66,51 +66,54 @@ void ABuptCharacter::MoveRight(float value)
 
 void ABuptCharacter::SpawnProjectile(TSubclassOf<AActor> ClassToSpawn)
 {
-	FVector HandLocation = GetMesh()->GetSocketLocation("Muzzle_01");
-
-	//设置line trace射线检测碰撞的类型
-	FCollisionObjectQueryParams ObjectQueryParams;
-	ObjectQueryParams.AddObjectTypesToQuery(ECC_WorldDynamic);
-	ObjectQueryParams.AddObjectTypesToQuery(ECC_WorldStatic);
-
-	FHitResult Hit;
-
-	//由于相机的旋转是与控制器的旋转绑定的，所以可以直接通过获得相机的位置和方向来获得准心的位置与朝向
-	FVector CameraLocation = CameraComp->GetComponentLocation();
-	FRotator CameraRotator = CameraComp->GetComponentRotation();
-
-	//射线的长度由角色的攻击范围决定
-	int AttackRange = 5000;
-	FVector End = CameraLocation + (CameraRotator.Vector() * AttackRange);
-
-	bool bBlockingHit = GetWorld()->LineTraceSingleByObjectType(Hit, CameraLocation, End, ObjectQueryParams);
-	//DrawDebugLine(GetWorld(), CameraLocation, End, FColor::Red, true, 2.0f, 0, 2.0f);
-
-
-	FTransform SpawnTM;
-
-	FRotator Bias;
-
-	if (bBlockingHit)
+	if (ensure(ClassToSpawn))
 	{
-		//如果射线被阻挡，说明遇到了障碍物，发生了碰撞
-		FVector HitPoint = Hit.ImpactPoint;
-		Bias = UKismetMathLibrary::FindLookAtRotation(HandLocation, HitPoint);
-		//DrawDebugSphere(GetWorld(), HitPoint, 20.0f, 16, FColor::Red, true);
+		FVector HandLocation = GetMesh()->GetSocketLocation("Muzzle_01");
+
+		//设置line trace射线检测碰撞的类型
+		FCollisionObjectQueryParams ObjectQueryParams;
+		ObjectQueryParams.AddObjectTypesToQuery(ECC_WorldDynamic);
+		ObjectQueryParams.AddObjectTypesToQuery(ECC_WorldStatic);
+
+		FHitResult Hit;
+
+		//由于相机的旋转是与控制器的旋转绑定的，所以可以直接通过获得相机的位置和方向来获得准心的位置与朝向
+		FVector CameraLocation = CameraComp->GetComponentLocation();
+		FRotator CameraRotator = CameraComp->GetComponentRotation();
+
+		//射线的长度由角色的攻击范围决定
+		int AttackRange = 5000;
+		FVector End = CameraLocation + (CameraRotator.Vector() * AttackRange);
+
+		bool bBlockingHit = GetWorld()->LineTraceSingleByObjectType(Hit, CameraLocation, End, ObjectQueryParams);
+		//DrawDebugLine(GetWorld(), CameraLocation, End, FColor::Red, true, 2.0f, 0, 2.0f);
+
+
+		FTransform SpawnTM;
+
+		FRotator Bias;
+
+		if (bBlockingHit)
+		{
+			//如果射线被阻挡，说明遇到了障碍物，发生了碰撞
+			FVector HitPoint = Hit.ImpactPoint;
+			Bias = UKismetMathLibrary::FindLookAtRotation(HandLocation, HitPoint);
+			//DrawDebugSphere(GetWorld(), HitPoint, 20.0f, 16, FColor::Red, true);
+		}
+		else
+		{
+			Bias = UKismetMathLibrary::FindLookAtRotation(HandLocation, End);
+		}
+
+
+		SpawnTM = FTransform(Bias, HandLocation);//para1表示获得控制器面向的方向，para2表示发射位置
+
+		FActorSpawnParameters SpawnParams;
+		SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;//不考虑发射物体是否会碰撞的问题
+		SpawnParams.Instigator = this;
+
+		GetWorld()->SpawnActor<AActor>(ClassToSpawn, SpawnTM, SpawnParams);
 	}
-	else
-	{
-		Bias = UKismetMathLibrary::FindLookAtRotation(HandLocation, End);
-	}
-
-
-	SpawnTM = FTransform(Bias, HandLocation);//para1表示获得控制器面向的方向，para2表示发射位置
-
-	FActorSpawnParameters SpawnParams;
-	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;//不考虑发射物体是否会碰撞的问题
-	SpawnParams.Instigator = this;
-
-	GetWorld()->SpawnActor<AActor>(ClassToSpawn, SpawnTM, SpawnParams);
 }
 
 
